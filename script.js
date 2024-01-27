f8b400b5cbcded6706a1d5fd501d55c5a58f089c
areso_0 <valentin.chiepperin@gmail.com>
2024-01-27 22:00:12 +0100

    mise a jour correction problème

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function playHoverSound() {
   var audio = new Audio("hover-sound.mp3");
   audio.play();
 }
+function playButtonClickSound() {
+  var audio = new Audio("vine-boom.mp3");
+  audio.play();
+}
+function playButtonClickSound(audioFileName) {
+  // Créer un objet Audio et spécifier le chemin du fichier audio
+  var audio = new Audio(audioFileName);
+
+  // Jouer le son
+  audio.play();
+}
